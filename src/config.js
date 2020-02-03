70454de6d27184bffb2af272fcf7a7345fc91ee2
Nedelin Yordanov <nedeliny@gmail.com>
2020-02-03 18:14:50 +0200

    Add Viewport management(camera), using pixi-viewport

--- src/config.js
+++ src/config.js
@@ -3,6 +3,9 @@ export default {
     width: window.innerWidth,
     height: window.innerHeight,
     backgroundColor: 0xfafafa,
+    worldWidth: 1000,
+    worldHeight: 500,
+    resizeTo: window,
   },
   assets: {
     root: '/',
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import logoBack from './assets/logo-back.png';
 import { Loader, Sprite, Application, utils } from 'pixi.js';
 import { TimelineMax } from 'gsap/all';
 import config from './config';
+import { Viewport } from 'pixi-viewport';
 
 class Game
 {
@@ -28,7 +29,7 @@ class Game
     // create a sprite with the logo asset as texture and add it to the stage
     const sprite = Sprite.from(logo);
 
-    this.app.stage.addChild(sprite);
+    this.viewport.addChild(sprite);
     sprite.scale.set(0.1);
     sprite.anchor.set(0.5);
 
@@ -55,10 +56,10 @@ class Game
   }
 
   /**
-	 * Create a Loader instance and add the game assets to the queue
-	 *
-	 * @return {Promise} Resolved when the assets files are downloaded and parsed into texture objects
-	 */
+     * Create a Loader instance and add the game assets to the queue
+     *
+     * @return {Promise} Resolved when the assets files are downloaded and parsed into texture objects
+     */
   loadAssets()
   {
     const loader = new Loader(this.config.root);
@@ -79,11 +80,11 @@ class Game
   }
 
   /**
-	 * Prerender our loaded textures, so that they don't need to be uploaded to the GPU the first time we use them.
-	 * Very helpful when we want to swap textures during an animation without the animation stuttering
-	 *
-	 * @return {Promise} Resolved when all queued uploads have completed
-	 */
+     * Prerender our loaded textures, so that they don't need to be uploaded to the GPU the first time we use them.
+     * Very helpful when we want to swap textures during an animation without the animation stuttering
+     *
+     * @return {Promise} Resolved when all queued uploads have completed
+     */
   prepareAssets()
   {
     const prepare = this.app.renderer.plugins.prepare;
@@ -98,32 +99,49 @@ class Game
   }
 
   /**
-	 * Initialize our Application instance
-	 *
-	 * @return {PIXI.Application}
-	 */
+     * Initialize our Application instance
+     *
+     * @return {PIXI.Application}
+     */
   createApp()
   {
     const app = new Application(this.config.view);
+    const viewport = new Viewport({
+      screenWidth: this.config.view.width,
+      screenHeight: this.config.view.height,
+      worldWidth: this.config.view.worldWidth,
+      worldHeight: this.config.view.worldHeight,
+      // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+      interaction: app.renderer.plugins.interaction,
+    });
 
     app.renderer.runners.resize.add({
       resize: this.resizeScene.bind(this),
     });
     document.body.appendChild(app.view);
 
+    app.stage.addChild(viewport);
+
+    viewport
+      .drag()
+      .pinch()
+      .wheel()
+      .decelerate();
+
     this.app = app;
+    this.viewport = viewport;
   }
 
   /**
-	 * Called after the browser window has been resized.
-	 * Implement game specific resize logic here
-	 * @param  {PIXI.Application} app The PIXI Appliaction instance
-	 * @param  {Number} width         The updated viewport width
-	 * @param  {Number} height        The updated viewport width
-	 */
+     * Called after the browser window has been resized.
+     * Implement game specific resize logic here
+     * @param  {PIXI.Application} app The PIXI Appliaction instance
+     * @param  {Number} width         The updated viewport width
+     * @param  {Number} height        The updated viewport width
+     */
   resizeScene(width = this.config.view.width, height = this.config.view.height)
   {
-    this.app.stage.children.forEach((child) =>
+    this.viewport.children.forEach((child) =>
     {
       child.x = width / 2;
       child.y = height / 2;

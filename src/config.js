9a105deeea034bd93a35ea185c8c48d976b4d829
Nedelin Yordanov <nedeliny@gmail.com>
2020-02-03 16:21:42 +0200

    Convert to OOP architecture, move config to separate file

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export default {
+	view: {
+		width: window.innerWidth,
+		height: window.innerHeight,
+		backgroundColor: 0xfafafa,
+	},
+	assets: {
+		root: '/',
+	}
+}
--- src/index.js
+++ src/index.js
@@ -2,83 +2,119 @@ import logo from './assets/logo.png';
 import logoBack from './assets/logo-back.png';
 import {Loader, Sprite, Texture, Renderer, Application, utils} from 'pixi.js';
 import { TimelineMax } from 'gsap/all';
+import config from './config';
 
-const config = {
-	view: {
-		width: window.innerWidth,
-		height: window.innerHeight,
-		backgroundColor: 0xfafafa,
-		resizeTo: window,
-	},
-	assets: {
-		root: '/',
+class Game {
+	constructor() {
+		this.config = config;
+
+		this.createApp();
+		this.init();
 	}
-};
 
-async function main() {
-	const {stage, renderer} = createApp();
+	async init() {
+		await this.loadAssets();
+		await this.prepareAssets();
 
-	await loadAssets();
-	await prepareAssets({stage, renderer});
+		// implement your game logic here
+		this.createScene();
+	}
 
-	const sprite = Sprite.from(logo);
-	const duration = 1.5;
+	createScene() {
+		// create a sprite with the logo asset as texture and add it to the stage
+		const sprite = Sprite.from(logo);
 
-	sprite.scale.set(0.1);
-	sprite.anchor.set(0.5)
-	stage.addChild(sprite);
+		this.app.stage.addChild(sprite);
+		sprite.scale.set(0.1);
+		sprite.anchor.set(0.5);
 
-	resizeScene({stage});
+		// update our scene to fit the screen
+		this.resizeScene();
 
-	new TimelineMax()
-	.to({}, 2, {})
-	.to(sprite.skew, duration/2, {x: 0, y: 1.568 })
-	.call(() => {
-		renderer.backgroundColor = 0xfffff5;
-		sprite.texture = utils.TextureCache[logoBack];
-	})
-	.to({}, 0.2, {})
-	.to(sprite.skew, duration/2, {x: 0, y: 3 })
+		this.spinCoin(sprite);
+	}
+
+	spinCoin(sprite) {
+		const duration = 1.5;
+
+		// create a simple coin flip animation
+		// and swap the sprite texture(to the coin back side) midway
+		new TimelineMax()
+		.to(sprite.skew, duration/2, {x: 0, y: 1.568 })
+		.call(() => {
+			this.app.renderer.backgroundColor = 0xfffff5;
+			sprite.texture = this.textures[logoBack];
+		})
+		.to(sprite.skew, duration/2, {x: 0, y: 3 })
+	}
+
+	/**
+	 * Create a Loader instance and add the game assets to the queue
+	 * 
+	 * @return {Promise} Resolved when the assets files are downloaded and parsed into texture objects
+	 */
+	loadAssets() {
+		const loader = new Loader(this.config.root);
+
+		return new Promise((resolve, reject)=>{
+			loader.add(logo);
+			loader.add(logoBack);
+
+			loader.load(resolve);
+		});
+	}
+
+	get textures() {
+		return utils.TextureCache;
+	}
+
+	/**
+	 * Prerender our loaded textures, so that they don't need to be uploaded to the GPU the first time we use them.
+	 * Very helpful when we want to swap textures during an animation without the animation stuttering
+	 * 
+	 * @return {Promise} Resolved when all queued uploads have completed
+	 */
+	prepareAssets() {
+		const prepare = this.app.renderer.plugins.prepare;
+
+		return new Promise((resolve)=>{
+			prepare.add( utils.TextureCache[logo])
+			prepare.add( utils.TextureCache[logoBack])
+
+			prepare.upload(resolve)
+		});
+	}
+
+	/**
+	 * Initialize our Application instance
+	 * 
+	 * @return {PIXI.Application}
+	 */
+	createApp() {
+		const app = new Application(this.config.view);
+
+		app.renderer.runners.resize.add({
+			resize: this.resizeScene.bind(this)
+		});
+		document.body.appendChild(app.view);
+
+		this.app = app;
+	}
+
+	/**
+	 * Called after the browser window has been resized.
+	 * Implement game specific resize logic here
+	 * @param  {PIXI.Application} app The PIXI Appliaction instance
+	 * @param  {Number} width         The updated viewport width
+	 * @param  {Number} height        The updated viewport width
+	 */
+	resizeScene(width = this.config.view.width, height = this.config.view.height) {
+		this.app.stage.children.forEach((child)=>{
+			child.x = width/2;
+			child.y = height/2;
+		})
+	}
+
 }
 
-function resizeScene({stage}, width = config.view.width, height = config.view.height) {
-	stage.children.forEach((child)=>{
-		child.x = width/2;
-		child.y = height/2;
-	})
-}
-
-function loadAssets() {
-	const loader = new Loader(config.root);
-
-	return new Promise((resolve, reject)=>{
-		loader.add(logo);
-		loader.add(logoBack);
-
-		loader.load(resolve);
-	});
-}
-
-function prepareAssets({stage, renderer}) {
-	return new Promise((resolve)=>{
-		const prepare = renderer.plugins.prepare;
-
-		prepare.add( utils.TextureCache[logo])
-		prepare.add( utils.TextureCache[logoBack])
-
-		prepare.upload(resolve)
-	});
-}
-
-function createApp() {
-	const app = new Application(config.view);
-
-	app.renderer.runners.resize.add({
-		resize: (...args)=>resizeScene(app, ...args)
-	});
-	document.body.appendChild(app.view);
-
-	return app;
-}
-
-document.addEventListener('DOMContentLoaded', main);
+document.addEventListener('DOMContentLoaded', new Game);

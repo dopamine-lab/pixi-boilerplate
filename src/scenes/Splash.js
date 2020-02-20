08e3daf0fb43dda0b1d0fbf46b32e0c9b1eebba6
Nedelin Yordanov <n.yordanov@dopamine.bg>
2020-02-20 12:17:36 +0200

    fix: use more descriptive syntax for animation loading

--- src/scenes/Splash.js
+++ src/scenes/Splash.js
@@ -28,34 +28,7 @@ export default class Splash extends Scene {
 
   preload() {
     // load the Wolf animation
-    return new Promise((resolve)=>animate.load(Wolf.stage, resolve, 'src/'));
-
-    // const images = {
-    //   tile11Cap: Assets.images.tile11Cap,
-    //   tile11EyebrowR: Assets.images.tile11EyebrowR,
-    //   tile11EyebrowL: Assets.images.tile11EyebrowL,
-    //   tile11EyeL: Assets.images.tile11EyeL,
-    //   tile11Nose: Assets.images.tile11Nose,
-    //   tile11EyeR: Assets.images.tile11EyeR,
-    //   tile11Mouth: Assets.images.tile11Mouth,
-    //   tile11Head: Assets.images.tile11Head,
-    //   tile11HeadShadow: Assets.images.tile11HeadShadow,
-    //   tile11HowlingEpaulet: Assets.images.tile11HowlingEpaulet,
-    //   tile11HowlingMouth: Assets.images.tile11HowlingMouth,
-    //   tile11HowlingEyebrow: Assets.images.tile11HowlingEyebrow,
-    //   tile11HowlingHead: Assets.images.tile11HowlingHead,
-    //   tile11HowlingPaw: Assets.images.tile11HowlingPaw,
-    //   tile11HowlingFeathers: Assets.images.tile11HowlingFeathers,
-    //   tile11FrameFront: Assets.images.tile11FrameFront,
-    //   tile11BodyNew: Assets.images.tile11BodyNew,
-    //   winFrameLight: Assets.images.winFrameLight,
-    //   tile11FrameBack: Assets.images.tile11FrameBack,
-    // };
-    // const sounds = {
-      
-    // };
-
-    // return super.preload({ images, sounds });
+    return new Promise((resolve)=>animate.load({stage: Wolf.stage, complete: resolve, basePath: 'src/'}));
   }
 
   onResize(width, height) { // eslint-disable-line no-unused-vars
